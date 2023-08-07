27d4eb4c952af478e508692e66aabfe7c6d12e66
David Petr <d.petr.cz@gmail.com>
2023-08-07 12:00:58 -0700

    Create playRound function, standardize playerInput Oops! Accidentally did a bunch between commits. This add starts by taking a player input and making only the first letter capitalized, to standardize the input for edge cases. Then, it passes both the result and the result of the random computer choice to the new playRound function,  which checks the player input against possible computer choices and displays the appropriate message. Sorry again for making this so long T_T

--- script.js
+++ script.js
@@ -8,5 +8,48 @@ function getComputerChoice() {
   return options[choice]
 }
 
-console.log(getComputerChoice());
+//Create an outside variable to pass to the playRound function
+function capitalize(playerInput) {
+  let lowerCase = playerInput.toLowerCase();
+  let playerInputStandardized = lowerCase.replace(lowerCase.charAt(0), lowerCase.charAt(0).toUpperCase())
+  return playerInputStandardized;
+}
 
+const playerInput = "PAPER"
+const computerSelection = getComputerChoice();
+const playerSelection = capitalize(playerInput);
+
+console.log(playRound(playerSelection, computerSelection))
+
+//Create Lose, Win, or Tie messages
+//Check if player selection matches rock, then paper, then scissors
+//Check the computer selection against that and return Lose, Win, or Tie
+
+function playRound(playerSelection, computerSelection) {
+  const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+  const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+  const tieMessage = `It's a Tie!`;
+
+  if (playerSelection === computerSelection) {
+    return tieMessage
+  } else if (playerSelection === "Rock") {
+    if (computerSelection === "Paper") {
+      return loseMessage
+    } else {
+      return winMessage
+    }
+  } else if (playerSelection === "Paper") {
+    if (computerSelection === "Scissors") {
+      return loseMessage
+    } else {
+      return winMessage
+    }
+  } else {
+    if (computerSelection === "Rock") {
+      return loseMessage
+    } else {
+      return winMessage
+    }
+  }
+
+}
