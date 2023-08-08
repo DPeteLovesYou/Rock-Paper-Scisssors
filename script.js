4ad9e849bb7886af330e826eb5d0eadb18e81539
David Petr <d.petr.cz@gmail.com>
2023-08-08 09:30:10 -0700

    Add playerInput prompt, move consts to top Ahh jeez another one where I did too many changes at once, sorry love you

--- script.js
+++ script.js
@@ -8,28 +8,30 @@ function getComputerChoice() {
   return options[choice]
 }
 
-//Create an outside variable to pass to the playRound function
+//Standardizes player input so that only the first letter is capitalized
 function capitalize(playerInput) {
   let lowerCase = playerInput.toLowerCase();
   let playerInputStandardized = lowerCase.replace(lowerCase.charAt(0), lowerCase.charAt(0).toUpperCase())
   return playerInputStandardized;
 }
 
-const playerInput = "PAPER"
+//Create an outside variable to pass to the playRound function
+const playerInput = prompt("Rock, Paper, or Scissors??")
 const computerSelection = getComputerChoice();
 const playerSelection = capitalize(playerInput);
 
-console.log(playRound(playerSelection, computerSelection))
 
-//Create Lose, Win, or Tie messages
-//Check if player selection matches rock, then paper, then scissors
-//Check the computer selection against that and return Lose, Win, or Tie
 
-function playRound(playerSelection, computerSelection) {
+  //Create Lose, Win, or Tie messages
   const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
   const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
   const tieMessage = `It's a Tie!`;
 
+  console.log(playRound(playerSelection, computerSelection));
+
+function playRound(playerSelection, computerSelection) {
+  //Check if player selection matches rock, then paper, then scissors
+  //Check the computer selection against that and return Lose, Win, or Tie
   if (playerSelection === computerSelection) {
     return tieMessage
   } else if (playerSelection === "Rock") {
@@ -51,5 +53,20 @@ function playRound(playerSelection, computerSelection) {
       return winMessage
     }
   }
-
 }
+
+  //Create a function called game() that keeps score of a 5 round game and reports a winner or loser at the end. 
+
+  //Call the playRound function 5 times
+  //Keep a computer score and a player score based on win messages
+  //Compare computer and player score and return win/lose/tie message.
+  
+  function game() {
+    const playerScore = 0;
+    const computerScore = 0;
+
+   playRound(playerSelection, computerSelection);
+
+   
+    return playerScore
+  }
