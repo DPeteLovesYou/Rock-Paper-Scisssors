4b2ab393d7ff4b3f3d66ac4d177bc68fa40ea3ab
David Petr <d.petr.cz@gmail.com>
2023-08-09 08:52:42 -0700

    Creates game(), working for a single call not loop

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Create outside variables to pass to the playRound function
+let playerInput = prompt("Rock, Paper, or Scissors??")
+const computerSelection = getComputerChoice();
+const playerSelection = capitalize(playerInput);
+
+//Create Lose, Win, or Tie messages
+const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+const tieMessage = `It's a Tie!`;
+
 // Create an array with the three options
 // Select an option at random
 // Return the selected option
@@ -12,22 +22,11 @@ function getComputerChoice() {
 function capitalize(playerInput) {
   let lowerCase = playerInput.toLowerCase();
   let playerInputStandardized = lowerCase.replace(lowerCase.charAt(0), lowerCase.charAt(0).toUpperCase())
+
   return playerInputStandardized;
 }
 
-//Create an outside variable to pass to the playRound function
-const playerInput = prompt("Rock, Paper, or Scissors??")
-const computerSelection = getComputerChoice();
-const playerSelection = capitalize(playerInput);
-
-
-
-  //Create Lose, Win, or Tie messages
-  const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
-  const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
-  const tieMessage = `It's a Tie!`;
-
-  console.log(playRound(playerSelection, computerSelection));
+console.log(game());
 
 function playRound(playerSelection, computerSelection) {
   //Check if player selection matches rock, then paper, then scissors
@@ -58,15 +57,22 @@ function playRound(playerSelection, computerSelection) {
   //Create a function called game() that keeps score of a 5 round game and reports a winner or loser at the end. 
 
   //Call the playRound function 5 times
+  //Use console.log() to display win/lose message after each round
   //Keep a computer score and a player score based on win messages
-  //Compare computer and player score and return win/lose/tie message.
+  //Compare computer and player score and return win/lose/tie message via 
+  //console.log()
   
   function game() {
-    const playerScore = 0;
-    const computerScore = 0;
+    let playerScore = 0;
+    let computerScore = 0;
 
-   playRound(playerSelection, computerSelection);
-
-   
-    return playerScore
+    if (playRound(playerSelection, computerSelection) == winMessage) {
+      playerScore++
+      console.log(winMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`)
+    } else {
+      computerScore++
+      console.log(loseMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    }
   }
