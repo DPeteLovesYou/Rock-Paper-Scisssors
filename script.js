092bd3d538175ec6a7a8ead20062e444e99cd9e5
David Petr <d.petr.cz@gmail.com>
2023-08-14 19:09:14 -0700

    I am not proud of the this code but it works Hacked together game() and it is technically working...so...yeah. Embarassing.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //Create outside variables to pass to the playRound function
-let playerInput = prompt("Rock, Paper, or Scissors??")
-const computerSelection = getComputerChoice();
-const playerSelection = capitalize(playerInput);
+let computerSelection //= getComputerChoice();
+let playerInput //= prompt("Rock, Paper or Scissors??");
+let playerSelection //= capitalize(playerInput);
 
 //Create Lose, Win, or Tie messages
-const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
-const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
-const tieMessage = `It's a Tie!`;
+// const loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+// const winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+// const tieMessage = `It's a Tie!`;
 
 // Create an array with the three options
 // Select an option at random
@@ -32,24 +32,24 @@ function playRound(playerSelection, computerSelection) {
   //Check if player selection matches rock, then paper, then scissors
   //Check the computer selection against that and return Lose, Win, or Tie
   if (playerSelection === computerSelection) {
-    return tieMessage
+    return "Tie"
   } else if (playerSelection === "Rock") {
     if (computerSelection === "Paper") {
-      return loseMessage
+      return "Lose"
     } else {
-      return winMessage
+      return "Win"
     }
   } else if (playerSelection === "Paper") {
     if (computerSelection === "Scissors") {
-      return loseMessage
+      return "Lose"
     } else {
-      return winMessage
+      return "Win"
     }
   } else {
     if (computerSelection === "Rock") {
-      return loseMessage
+      return "Lose"
     } else {
-      return winMessage
+      return "Win"
     }
   }
 }
@@ -66,13 +66,125 @@ function playRound(playerSelection, computerSelection) {
     let playerScore = 0;
     let computerScore = 0;
 
-    if (playRound(playerSelection, computerSelection) == winMessage) {
+    computerSelection = getComputerChoice();
+    playerInput = prompt("Rock, Paper or Scissors??");
+    playerSelection = capitalize(playerInput);
+
+    let loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    let winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+    let tieMessage = `It's a Tie!`;
+
+    first = playRound(playerSelection, computerSelection);
+    if (first == "Win") {
       playerScore++
       console.log(winMessage);
       console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`)
-    } else {
+    } else if (first == "Lose") {
       computerScore++
       console.log(loseMessage);
       console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    } else {
+      console.log(tieMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    }
+
+    getComputerChoice();
+    computerSelection = getComputerChoice();
+    playerInput = prompt("Rock, Paper or Scissors??");
+    playerSelection = capitalize(playerInput);
+
+    loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+    tieMessage = `It's a Tie!`;
+
+    second = playRound(playerSelection, computerSelection);
+    if (second == "Win") {
+      playerScore++
+      console.log(winMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`)
+    } else if (second == "Lose") {
+      computerScore++
+      console.log(loseMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    } else {
+      console.log(tieMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    }
+
+    getComputerChoice();
+    computerSelection = getComputerChoice();
+    playerInput = prompt("Rock, Paper or Scissors??");
+    playerSelection = capitalize(playerInput);
+
+    loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+    tieMessage = `It's a Tie!`;
+
+    third = playRound(playerSelection, computerSelection);
+    if (third == "Win") {
+      playerScore++
+      console.log(winMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`)
+    } else if (third == "Lose") {
+      computerScore++
+      console.log(loseMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    } else {
+      console.log(tieMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    }
+
+    getComputerChoice();
+    computerSelection = getComputerChoice();
+    playerInput = prompt("Rock, Paper or Scissors??");
+    playerSelection = capitalize(playerInput);
+
+    loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+    tieMessage = `It's a Tie!`;
+
+    fourth = playRound(playerSelection, computerSelection);
+    if (fourth == "Win") {
+      playerScore++
+      console.log(winMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`)
+    } else if (fourth == "Lose") {
+      computerScore++
+      console.log(loseMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    } else {
+      console.log(tieMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    }
+
+    getComputerChoice();
+    computerSelection = getComputerChoice();
+    playerInput = prompt("Rock, Paper or Scissors??");
+    playerSelection = capitalize(playerInput);
+
+    loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+    tieMessage = `It's a Tie!`;
+
+    fifth = playRound(playerSelection, computerSelection);
+    if (fifth == "Win") {
+      playerScore++
+      console.log(winMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`)
+    } else if (fifth == "Lose") {
+      computerScore++
+      console.log(loseMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    } else {
+      console.log(tieMessage);
+      console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
+    }
+
+    if (playerScore == computerScore) {
+      console.log("You tied with the computer!!")
+    } else if (playerScore > computerScore) {
+      console.log("Victory! You beat the computer")
+    } else {
+      console.log("You Lost! Computers are smart")
     }
   }
