c75e3dc1bfee44bb78eaf07afcb9ce073107c93d
David Petr <d.petr.cz@gmail.com>
2023-08-30 13:48:33 -0700

    Cleanup and remove unused code

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function endGame() {
 
   const playAgain = document.createElement('button')
   playAgain.classList.add('playAgain')
-  playAgain.style.cssText = 'width: 200px; height: 100px; background-color: orange;'
+  playAgain.style.cssText = 'width: 200px; height: 100px; background-color: orange'
   playAgain.textContent = 'Play Again?'
 
   container.appendChild(playAgain);
@@ -153,9 +153,6 @@ function getTieSplash() {
   return `We Tie?? ${options[choice]}`
 }
 
-// Create an array with the three options
-// Select an option at random
-// Return the selected option
 function getComputerChoice() {
   const options = ["Rock", "Paper", "Scissors"]
   const choice = Math.floor(Math.random() * options.length)
@@ -164,8 +161,6 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  //Check if player selection matches rock, then paper, then scissors
-  //Check the computer selection against that and return Lose, Win, or Tie
   if (playerSelection === computerSelection) {
     return "Tie"
   } else if (playerSelection === "Rock") {
@@ -188,43 +183,3 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 }
-
-  //Create a function called game() that keeps score of a 5 round game and reports a winner or loser at the end. 
-  
-  function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let gameNumber = 0;
-
-    // for (i = gameNumber; i < 5; i++) {
-    //   computerSelection = getComputerChoice();
-    //   playerInput = prompt("Rock, Paper or Scissors??");
-    //   playerSelection = capitalize(playerInput);
-  
-    //   let loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
-    //   let winMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
-    //   let tieMessage = `It's a Tie!`;
-  
-    //   first = playRound(playerSelection, computerSelection);
-    //   if (first == "Win") {
-    //     playerScore++
-    //     console.log(winMessage);
-    //     console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`)
-    //   } else if (first == "Lose") {
-    //     computerScore++
-    //     console.log(loseMessage);
-    //     console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
-    //   } else {
-    //     console.log(tieMessage);
-    //     console.log(`Your score is ${playerScore}, the computer score is ${computerScore}`);
-    //   }
-    // }
-
-    // if (playerScore == computerScore) {
-    //   console.log("You tied with the computer!!")
-    // } else if (playerScore > computerScore) {
-    //   console.log("Victory! You beat the computer")
-    // } else {
-    //   console.log("You Lost! Computers are smart")
-    // }
-  }
