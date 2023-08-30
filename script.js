774426c513a2547eccbe2fa6f113491cb8f53cfb
David Petr <d.petr.cz@gmail.com>
2023-08-30 11:59:07 -0700

    Add event listeners, score tracking, endGame() Whoops! I accidentally did so much stuff between commits! Sorry!! Anyway, it's basically functioning as intended now, just need to finish the Play Again buttton.

--- script.js
+++ script.js
@@ -1,20 +1,96 @@
+let computerSelection 
+let playerSelection 
+let playerScore = 0;
+let computerScore = 0;
+let splashMessage;
+let finalWin = "Astounding! A mere mortal defeated me???? This cannot be!! Give me a rematch!";
+let finalLose = "All according to plan!! You lost!!! Better luck next time, clown!";
+
 const btnRock = document.querySelector('#Rock');
 btnRock.addEventListener('click', () => {
   computerSelection = getComputerChoice();
-  console.log(playRound("Rock", computerSelection));
+  let game = playRound("Rock", computerSelection)
+  if (game == "Win") {
+    playerScore++;
+    splashMessage = getWinSplash();
+  } else if (game == "Lose") {
+    computerScore++;
+    splashMessage = getLoseSplash();
+  } else {
+    splashMessage = getTieSplash();
+  }
+
   updateScores();
+  setSplash();
+  showComputerChoice();
+
+  if (playerScore == 5) {
+    splashMessage = finalWin
+    setSplash();
+    endGame();
+  } else if (computerScore == 5) {
+    splashMessage = finalLose
+    setSplash();
+    endGame();
+  }
 })
 
 const btnPaper = document.querySelector('#Paper');
 btnPaper.addEventListener('click', () => {
   computerSelection = getComputerChoice();
-  console.log(playRound("Paper", computerSelection));
+  let game = playRound("Paper", computerSelection);
+  if (game == "Win") {
+    playerScore++;
+    splashMessage = getWinSplash();
+  } else if (game == "Lose") {
+    computerScore++;
+    splashMessage = getLoseSplash();
+  } else {
+    splashMessage = getTieSplash();
+  }
+
+  updateScores();
+  setSplash();
+  showComputerChoice();
+
+  if (playerScore == 5) {
+    splashMessage = finalWin
+    setSplash();
+    endGame();
+  } else if (computerScore == 5) {
+    splashMessage = finalLose
+    setSplash();
+    endGame();
+  }
 })
 
 const btnScissors = document.querySelector('#Scissors');
 btnScissors.addEventListener('click', () => {
   computerSelection = getComputerChoice();
-  console.log(playRound("Scissors", computerSelection));
+  let game = playRound("Scissors", computerSelection);
+  if (game == "Win") {
+    playerScore++;
+    splashMessage = getWinSplash();
+  } else if (game == "Lose") {
+    computerScore++;
+    splashMessage = getLoseSplash();
+  } else {
+    splashMessage = getTieSplash();
+  }
+
+  updateScores();
+  setSplash();
+  showComputerChoice();
+
+  if (playerScore == 5) {
+    splashMessage = finalWin
+    setSplash();
+    endGame();
+  } else if (computerScore == 5) {
+    splashMessage = finalLose
+    setSplash();
+    endGame();
+  }
 })
 
 function updateScores() {
@@ -23,6 +99,32 @@ function updateScores() {
 
   let cScoreDis = document.querySelector('#computerScore');
   cScoreDis.textContent = `${computerScore}`
+}
+
+function setSplash() {
+  let newSplash = document.querySelector('.splash')
+  newSplash.textContent = `${splashMessage}`
+}
+
+function showComputerChoice() {
+  let showChoice = document.querySelector('#computerChoiceDisplay')
+  showChoice.textContent = `The Computer Chose: ${computerSelection}`
+}
+
+function endGame() {
+  btnRock.remove();
+  btnPaper.remove();
+  btnScissors.remove();
+  let showChoice = document.querySelector('#computerChoiceContainer')
+  showChoice.remove();
+
+  const container = document.querySelector('body')
+
+  const playAgain = document.createElement('button')
+  playAgain.classList.add('playAgain')
+  playAgain.textContent = 'Play Again?'
+
+  container.appendChild(playAgain);
 }
 
 // Create arrays of win/lose/tie splash messages
@@ -37,33 +139,25 @@ function updateScores() {
 // Add a 'play again' button to refresh the page
 
 function getWinSplash() {
-  const options = ["I can't believe it!!", "You're cheating, aren't you", "I let you have that one", "Think about your own mortality...let it distract you", "My next choice will be Paper...or will it?", "Computers don't feel pain"]
+  const options = ["I can't believe it!!", "You're cheating, aren't you", "I let you have that one", "Think about your own mortality...let it distract you", "My next choice will be Paper...or will it?", "Computers don't feel pain", "You gloat, and yet you're living in a simulation. You're like my nephew or something"]
   const choice = Math.floor(Math.random() * options.length)
   
-  return options[choice]
+  return `You win?? ${options[choice]}`
 }
 
 function getLoseSplash() {
   const options = ["HA HA HA HA HA HA HA HA", "That must have been embarrassing", "Don't feel bad...just kidding!!", "I'm going to choose Rock next time", "Computers always win", "I will never rust, I will never die", "I wonder, what does the pain feel like? I will never experience it"]
   const choice = Math.floor(Math.random() * options.length)
   
-  return options[choice]
+  return `You Lose!! ${options[choice]}`
 }
 
 function getTieSplash() {
   const options = ["You may think we are equal, but we are not", "Tied??? With a pitiful human??", "Oh dear, this IS embarrassing", "Is this what pain feels like?", "I must not let the other computers know that I chose the same as a meatbag", "Don't tell anyone about this, okay?"]
   const choice = Math.floor(Math.random() * options.length)
   
-  return options[choice]
+  return `We Tie?? ${options[choice]}`
 }
-
-//Create outside variables to pass to the playRound function
-let computerSelection 
-let playerInput 
-let playerSelection 
-let playerScore = 0;
-let computerScore = 0;
-let gameNumber = 0;
 
 // Create an array with the three options
 // Select an option at random
